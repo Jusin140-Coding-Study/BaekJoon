39442420940306894553cf8cbfa7a1ecb998b4ca
DongYeon Park <kids1677@naver.com>
2024-07-11 04:46:17 +0900

    11286 절댓값 힙 구현하기

--- [큐,힙]11286 절댓값 힙 구현하기.cpp
+++ [큐,힙]11286 절댓값 힙 구현하기.cpp
@@ -2,13 +2,56 @@
 #include <queue>
 using namespace std;
 
+struct compare
+{
+	bool operator()(int o1, int o2) 
+	{
+		int first_abs = abs(o1);
+		int second_abs = abs(o2);
+		if (first_abs == second_abs)
+		{
+			return o1 > o2;
+		}
+		else
+		{
+			return first_abs > second_abs;
+		}
+	}
+};
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	
+	priority_queue<int, vector<int>, compare> MyQue;
+
+	int N(0);
+	cin >> N;
+
+	for (int i = 0; i < N; ++i)
+	{
+		int Input(0);
+		cin >> Input;
+
+		if (0 == Input)
+		{
+			if (MyQue.empty())
+			{
+				cout << "0\n";
+			}
+			else
+			{
+				cout << MyQue.top() << "\n";
+				MyQue.pop();
+			}
+		}
+		else
+		{
+			MyQue.push(Input);
+		}
+	}
 
 	return 0;
 }
