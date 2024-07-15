3b2e09c63256d23a88678d6d5e7d0215ad3231fa
박동연 <kids1677@naver.com>
2024-07-15 16:46:42 +0900

    gitignore 추가

--- 05. 탐색/5-3 이진 탐색/[골드1]★1300_k 번째 수 .cpp
+++ 05. 탐색/5-3 이진 탐색/[골드1]★1300_k 번째 수 .cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main()
@@ -9,8 +8,34 @@ int main()
 	cout.tie(NULL);
 
 	int N = 0;
-	int M = 0;
+	int K = 0;
 
 	cin >> N;
-	vector<int> V(N, 0);
+	cin >> K;
+	
+	int low = 1;
+	int high = K; // B[K]는 2차원 배열에서 항상 k보다 작다.
+
+	while (low < high)
+	{
+		int mid = (low + high) / 2;
+		int count = 0;
+
+		for (int i = 1; i <= N; ++i)
+		{
+			// i행에서 mid 보다 낮은 수의 개수를 모두 합한다.
+			count += min(mid / i, N);
+		}
+
+		if (count < K)
+		{
+			low = mid + 1;
+		}
+		else
+		{
+			high = mid;
+		}
+	}
+
+	cout << high;
 }
