a420ebedb7a4c949954d28f3cc8f24304c14175a
박동연 <kids1677@naver.com>
2024-07-19 22:11:46 +0900

    스터디 3-1주차 (~ing 이분그래프)

---
+++ [골드1]21568_Ax+By=C.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 현재 문제에 문제가있어 채점이 불가능
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N = 0;
+	cin >> N;
+
+	return 0;
+}
