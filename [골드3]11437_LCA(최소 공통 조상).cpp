d73724989522d8c45e71094cbca11d0e0a643c68
박동연 <kids1677@naver.com>
2024-08-01 21:50:43 +0900

    스터디 6-2주차(~ 최소 공통 조상 ing)

---
+++ [골드3]11437_LCA(최소 공통 조상).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+// 세그먼트 트리
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N; int M;
+	cin >> N >> M;
+
+	return 0;
+}
