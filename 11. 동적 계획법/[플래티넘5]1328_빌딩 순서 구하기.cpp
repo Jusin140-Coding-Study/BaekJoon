073f85af5759f184d15e94a193ad94caa182924c
DongYeon Park <kids1677@naver.com>
2024-08-15 01:23:08 +0900

    파일 정리

---
+++ 11. 동적 계획법/[플래티넘5]1328_빌딩 순서 구하기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N = 0; int M = 0;
+	cin >> N >> M;
+
+	return 0;
+}
