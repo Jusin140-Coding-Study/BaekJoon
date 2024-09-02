4cfc62dd09bea1e2d8bc6358c763f7df502c73e1
박동연 <kids1677@naver.com>
2024-09-02 16:44:25 +0900

    BFS, DFS

--- [실버1]2178_미로 탐색.cpp
+++ [실버1]2178_미로 탐색.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <queue>
-#include <algorithm>
 using namespace std;
+
+static int N, M;
+typedef pair<int, int> Index;
+static int iResult = 0;
+static vector<vector<int>> Graph;
+static vector<vector<bool>> Visited;
+void BFS(int i, int j);
 
 int main()
 {
@@ -9,8 +15,64 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int N, M;
 	cin >> N >> M;
 
+	Graph = vector<vector<int>>(N, vector<int>(M, -1));
+	Visited = vector<vector<bool>>(N, vector<bool>(M));
+
+	for (int i = 0; i < N; ++i)
+	{
+		string strInput;
+		cin >> strInput;
+
+		for (int j = 0; j < M; ++j)
+			Graph[i][j] = strInput[j] - '0';
+	}
+
+	BFS(0, 0);
+	cout << iResult;
+
 	return 0;
+}
+
+bool Check(int i, int j, queue<Index>& q, int iDepth)
+{
+	if (i >= N || j >= M || i < 0 || j < 0) return false;
+	if (Visited[i][j]) return false;
+	if (0 == Graph[i][j]) return false;
+
+	Visited[i][j] = true;
+	Graph[i][j] = iDepth + 1;
+
+	if (i == N - 1 && j == M - 1) 
+	{ 
+		iResult = Graph[i][j];
+		return true; 
+	}
+
+	q.push(make_pair(i, j));
+
+	return false;
+}
+
+void BFS(int i, int j)
+{
+	queue<Index> Q;
+	Q.push(make_pair(i, j));
+
+	Visited[i][j] = true;
+
+	while (!Q.empty())
+	{
+		Index curNode = Q.front();
+		Q.pop();
+		int x = curNode.first;
+		int y = curNode.second;
+		int iDepth = Graph[x][y];
+
+		if (Check(x + 1, y, Q, iDepth)) break;
+		if (Check(x, y + 1, Q, iDepth)) break;
+		if (Check(x - 1, y, Q, iDepth)) break;
+		if (Check(x, y - 1, Q, iDepth)) break;
+	}
 }
